255427578b56d7b9e0322e341ce9bcedc7d3711a
Thaynara Falcão <falcaoaudiovisual7@gmail.com>
2026-05-18 10:50:54 -0300

    nutri page

---
+++ script.js
@@ -0,0 +1,60 @@
+/* =========================
+ARQUIVO: script.js
+========================= */
+
+window.addEventListener("scroll", () => {
+
+  const header = document.querySelector(".header");
+
+  if(window.scrollY > 50){
+    header.style.background = "rgba(8,47,124,0.98)";
+  } else{
+    header.style.background = "rgba(8,47,124,0.90)";
+  }
+
+});
+
+/* Scroll suave extra */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+  anchor.addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    target.scrollIntoView({
+      behavior: "smooth"
+    });
+
+  });
+
+});
+
+/* =========================================
+ADICIONE NO FINAL DO script.js
+========================================= */
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+
+});
+
+/* FECHAR MENU AO CLICAR */
+
+document.querySelectorAll(".menu a").forEach(link => {
+
+  link.addEventListener("click", () => {
+
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+
+  });
+
+});
